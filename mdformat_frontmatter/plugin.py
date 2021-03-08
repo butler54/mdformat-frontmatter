d1c96376572582ff2c0470b9962abd4703fa6233
Chris Butler <chris@thebutlers.me>
2021-03-08 11:47:50 +1100

    fix:Remove pre-commit check to allow bootstrapping
    Signed-off-by: Chris Butler <chris@thebutlers.me>

--- mdformat_frontmatter/plugin.py
+++ mdformat_frontmatter/plugin.py
@@ -1,14 +1,15 @@
 from typing import List, Optional, Tuple
 
 from markdown_it import MarkdownIt
-from mdit_py_plugins.front_matter import front_matter_plugin
 from markdown_it.token import Token
 from mdformat.renderer import MDRenderer
+from mdit_py_plugins.front_matter import front_matter_plugin
 
 
 def update_mdit(mdit: MarkdownIt) -> None:
     """Update the parser, e.g. by adding a plugin: `mdit.use(myplugin)`"""
     mdit.use(front_matter_plugin)
+
 
 def render_token(
     renderer: MDRenderer,
@@ -22,15 +23,15 @@ def render_token(
     :returns: (text, index) where index is of the final "consumed" token
     """
     return None
-    if tokens[index].type != "front_matter": 
+    if tokens[index].type != "front_matter":
         return None
     else:
         return None
-    if tokens[index].type != "front_matter": 
+    if tokens[index].type != "front_matter":
         return None
     print(tokens[index])
-    content = ''
-    while index < len(tokens) and tokens[index].type == 'front_matter':
+    content = ""
+    while index < len(tokens) and tokens[index].type == "front_matter":
         # Not sure
 
         index += 1
