a7bbe7e754fcc2c09ef3b3f0dbeca56b5b039e7e
Chris Butler <chris@thebutlers.me>
2021-04-14 12:41:29 +1000

    feat: Revised and updated for mdformat 0.7
    Signed-off-by: Chris Butler <chris@thebutlers.me>

--- mdformat_frontmatter/__init__.py
+++ mdformat_frontmatter/__init__.py
@@ -1,5 +1,5 @@
 """An mdformat plugin for parsing / ignoring frontmatter."""
 
-__version__ = "0.2.2"
+__version__ = "0.3.1"
 
 from .plugin import POSTPROCESSORS, RENDERERS, update_mdit  # noqa: F401
